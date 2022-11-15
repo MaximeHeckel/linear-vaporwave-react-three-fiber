a3123ae7cf0deff1c89d9a5765396e6cac56e46c
ChristianOConnor <warpanomaly@gmail.com>
2022-11-14 23:05:33 -0500

    initial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { demos } from '@/lib/demos';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="space-y-6">
+      <div className="space-y-8 text-white">
+      </div>
+    </div>
+  );
+}
